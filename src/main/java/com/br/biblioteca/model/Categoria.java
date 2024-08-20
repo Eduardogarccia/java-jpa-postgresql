10c8b334d469654af87d453fd730844ab461dbff
Eduardo Garcia <eduardogarcia@Laptop-de-Eduardo.local>
2024-08-20 20:18:46 -0300

    atualização

---
+++ src/main/java/com/br/biblioteca/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.br.biblioteca.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_categoria", nullable = false)
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
